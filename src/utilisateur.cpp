e2c346e4d1457632add4a1eb30cbaf6f908683c1
RACHDI MOHAMED p1805378 <p1805378@univ-lyon1.fr>
2021-09-17 15:16:15 +0200

    Ajout de la classe utilisateur

---
+++ src/utilisateur.cpp
@@ -0,0 +1,6 @@
+#include "utilisateur.h"
+
+utilisateur::utilisateur()
+{
+
+}
---
+++ src/utilisateur.h
@@ -0,0 +1,21 @@
+#ifndef UTILISATEUR_H_INCLUDED
+#define UTILISATEUR_H_INCLUDED
+
+#include "voiture.h"
+
+class utilisateur
+{
+private:
+    int age;
+    char sexe;
+    string nom;
+    bool handicap;
+    Voiture voiture;
+
+public:
+    utilisateur();
+    ~utilisateur();
+
+};
+
+#endif // UTILISATEUR_H_INCLUDED
