68a4402187cc381171e25610545f7495ca6d3d6e
Rami <rami.mehaibia@etu.univ-lyon1.fr>
2021-10-18 20:44:27 +0200

    realisation d'un probleme sur git

---
+++ src/Voiture.h
@@ -0,0 +1,29 @@
+#ifndef __VOITURE__
+#define __VOITURE__
+
+#include <string>
+#include "Date.h"
+#include "Agent.h"
+
+
+using namespace std;
+
+class Voiture:public Agent{
+public:
+    Voiture();
+    Voiture(Voiture & v);
+    Voiture(string immat);
+    ~Voiture();
+    string getImat() const;
+
+    void Boucle();
+    void negociation(string id_destinataire, int compteur);
+   
+    void premierMessage(string id_destinataire);
+    void processusNegocition();
+
+private:
+    string immatriculation;
+};
+
+#endif
